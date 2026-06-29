aee452430a42a52d06e860acc06577cbc8cb3d36
Danilo Mendes <danilodev.br@gmail.com>
2026-06-29 19:53:45 -0300

    Add Spring Boot API foundation project

---
+++ 01-api-foundation-lab/order-api-foundation/src/main/java/br/com/danilo/orderfoundation/OrderApiFoundationApplication.java
@@ -0,0 +1,13 @@
+package br.com.danilo.orderfoundation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApiFoundationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApiFoundationApplication.class, args);
+	}
+
+}
---
+++ 01-api-foundation-lab/order-api-foundation/src/test/java/br/com/danilo/orderfoundation/OrderApiFoundationApplicationTests.java
@@ -0,0 +1,14 @@
+package br.com.danilo.orderfoundation;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class OrderApiFoundationApplicationTests {
+
+	@Test
+	void applicationClassExists() {
+		assertThat(OrderApiFoundationApplication.class).isNotNull();
+	}
+
+}
